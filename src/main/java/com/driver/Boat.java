00da2a2334ff712a3f701ae1ccadc8a1ab880208
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2022-12-17 19:01:52 +0530

    try

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,13 +1,21 @@
 package com.driver;
 
 public class Boat {
-//    String name = "boat";
-//    int capacity = 100;
-//    public String getVehicleName(){
-//        return this.name;
-//    }
-//
-//    public int getVehicleCapacity(){
-//        return this.capacity;
-//    }
+    private String name;
+    private int capacity;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getVehicleName(){
+        return this.name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getVehicleCapacity(){
+        return this.capacity;
+    }
 }
